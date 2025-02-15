4a6d0503bbe72c42445af5dfd6f3325b2676ebf9
abu <rahatit486@gmail.com>
2025-02-15 19:44:54 +0530

    css fixed

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,42 @@
 <script setup>
 
 
-import MyPackage from './components/MyPackage.vue';
-import Dashboard from './pages/Dashboard.vue';
+import Sidebar from './components/layout/Sidebar.vue';
+import HeaderLogo from './components/ui/HeaderLogo.vue';
+import Navbar from './components/layout/Navbar.vue';
+import FooterBottom from './components/layout/FooterBottom.vue';
+
+
 
 </script>
 
 <template>
 
-    <router-view />
+    <!-- <router-view /> -->
+     <!-- <DefaultLayout /> -->
+     <main > 
+
+<div class="wrapper">
+    <Sidebar />
+    <div class="main-panel">
+        <div class="main-header">
+            <div class="main-header-logo">
+               <!-- <HeaderLogo /> -->
+            </div>
+            <Navbar />
+        </div>
+        <div class="container">
+          <div class="page-inner">
+            <router-view />
+          </div>
+        </div>
+       
+        <FooterBottom />
+    </div>
+    
+</div>
+
+</main>
    
 </template>
 
