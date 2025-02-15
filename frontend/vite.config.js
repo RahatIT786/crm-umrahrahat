c8cbba8a3d92e50a319d792233e0ab24ab7fad8a
abu <rahatit486@gmail.com>
2025-02-15 13:06:44 +0530

    build2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/frontend/dist/', // Ensures correct relative paths
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
